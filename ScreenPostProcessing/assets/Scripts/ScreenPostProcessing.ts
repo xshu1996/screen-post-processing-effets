f543595420f757c2741253657f371c35e70aa1ef
xshu <825947170@qq.com>
2021-08-16 22:08:13 +0800

    feat: 更新发光特效， 兼容图集图片的渲染

--- ScreenPostProcessing/assets/Scripts/ScreenPostProcessing.ts
+++ ScreenPostProcessing/assets/Scripts/ScreenPostProcessing.ts
@@ -47,6 +47,25 @@ export class ScreenPostProcessing extends cc.Component {
         return this.effectType;
     }
 
+    // uv里面有8个值，分别是左下，右下，左上，右上4个点的x和y坐标，
+    public static getUVOffset(frame: cc.SpriteFrame) {
+        if (!cc.isValid(frame)) return null;
+
+        const ret = {
+            isRotated: 0,
+            uvOffset: new cc.Vec4(0, 0, 0, 0),
+        };
+
+        ret.uvOffset.x = frame.uv[0];
+        ret.uvOffset.y = frame.uv[1];
+        ret.uvOffset.z = frame.uv[6];
+        ret.uvOffset.w = frame.uv[7];
+
+        ret.isRotated = frame.isRotated() ? 1.0 : 0.0;
+
+        return ret;
+    }
+
     public static getRenderTexture(renderParam: IRenderParam): cc.RenderTexture {
         let { renderNode, frameSize, forceSnapShot = false, isClear = false } = renderParam;
         const node: cc.Node = this._getShotCameraNode();
