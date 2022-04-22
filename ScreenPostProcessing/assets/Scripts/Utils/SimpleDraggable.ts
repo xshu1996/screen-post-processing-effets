db3082cb915d208c1f770c85ae088c0d46db7a30
shuxiang <shux21091301@gulangkj.com>
2022-04-22 11:40:10 +0800

    refactor 格式化代码风格

--- ScreenPostProcessing/assets/Scripts/Utils/SimpleDraggable.ts
+++ ScreenPostProcessing/assets/Scripts/Utils/SimpleDraggable.ts
@@ -1,49 +1,61 @@
-const { ccclass, property } = cc._decorator;
+const { ccclass } = cc._decorator;
 
 @ccclass
-export default class SimpleDraggable extends cc.Component {
+export default class SimpleDraggable extends cc.Component
+{
     protected _touchOffset: cc.Vec2 = cc.Vec2.ZERO;
     protected _isDragging: boolean = false;
     protected _moveCallback: (pos: cc.Vec2) => void = null;
-    
-    onLoad() {
-        this.node.on(cc.Node.EventType.TOUCH_START, this.OnTouchStart.bind(this));
-        this.node.on(cc.Node.EventType.TOUCH_MOVE, this.OnTouchMove.bind(this));
-        this.node.on(cc.Node.EventType.TOUCH_END, this.OnTouchEnd.bind(this));
-        this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.OnTouchEnd.bind(this));
+
+    protected onLoad(): void
+    {
+        this._addEvent();
     }
 
-    public Setup(moveCallback: (pos: cc.Vec2) => void) {
+    public setup(moveCallback: (pos: cc.Vec2) => void): void
+    {
         this._moveCallback = moveCallback;
     }
 
-    protected OnTouchStart(e: cc.Event.EventTouch) {
+    protected onTouchStart(e: cc.Event.EventTouch): void
+    {
         let touchWorldPos = e.getLocation();
         let nodeWorldPos = this.node.convertToWorldSpaceAR(cc.Vec2.ZERO);
         this._touchOffset = nodeWorldPos.sub(touchWorldPos);
         this._isDragging = true;
-        this._moveCallback && this._moveCallback(this.node.position);
+        this._moveCallback && this._moveCallback(this.node.getPosition());
     }
 
-    protected OnTouchMove(e: cc.Event.EventTouch) {
+    protected onTouchMove(e: cc.Event.EventTouch): void
+    {
         if (!this._isDragging)
             return;
 
         let touchWorldPos = e.getLocation();
-        this.TraceTouchPos(touchWorldPos);
-        this._moveCallback && this._moveCallback(this.node.position);
+        this.traceTouchPos(touchWorldPos);
+        this._moveCallback && this._moveCallback(this.node.getPosition());
     }
 
-    protected OnTouchEnd(e: cc.Event.EventTouch) {
+    protected onTouchEnd(e: cc.Event.EventTouch): void
+    {
         if (!this._isDragging)
             return;
 
         this._isDragging = false;
     }
 
-    protected TraceTouchPos(worldPos: cc.Vec2) {
+    protected traceTouchPos(worldPos: cc.Vec2)
+    {
         let nodeWorldPos = worldPos.add(this._touchOffset);
         let localPos = this.node.parent.convertToNodeSpaceAR(nodeWorldPos);
-        this.node.position = localPos;
+        this.node.setPosition(localPos);
+    }
+
+    private _addEvent(): void
+    {
+        this.node.on(cc.Node.EventType.TOUCH_START, this.onTouchStart.bind(this));
+        this.node.on(cc.Node.EventType.TOUCH_MOVE, this.onTouchMove.bind(this));
+        this.node.on(cc.Node.EventType.TOUCH_END, this.onTouchEnd.bind(this));
+        this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.onTouchEnd.bind(this));
     }
 }
