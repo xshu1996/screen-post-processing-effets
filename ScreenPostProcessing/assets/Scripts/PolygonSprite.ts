0c0c5d95a60edaf46423af431b313dcfd9d52ce0
shuxiang <shux21091301@gulangkj.com>
2022-04-24 19:09:08 +0800

    feat 新增多边形编辑扩展

--- ScreenPostProcessing/assets/Scripts/PolygonSprite.ts
+++ ScreenPostProcessing/assets/Scripts/PolygonSprite.ts
@@ -1,11 +1,30 @@
 import PolygonSpriteAssembler from "./custom_assembler/base_assembler/PolygonSpriteAssembler";
 
-const { ccclass, property } = cc._decorator;
+const { ccclass, property, inspector, menu, executeInEditMode } = cc._decorator;
+
 @ccclass
+@executeInEditMode
 export default class PolygonSprite extends cc.Sprite
 {
+    @property(cc.Texture2D)
+    _texture: cc.Texture2D = null;
+    @property(cc.Texture2D)
+    get texture() {
+        return this._texture;
+    }
+    set texture(val: cc.Texture2D) {
+        if (!val)
+        {
+            return;
+        }
+        this._texture = val;
+        let l = -val.width/2, b = -val.height/2, t = val.height/2, r = val.width/2;
+        this.polygon = [cc.v2(l, b), cc.v2(r, b), cc.v2(r, t), cc.v2(l, t)];
+        this.node.width = val.width; this.node.height = val.height;
+    }
+
     @property({ type: [cc.Vec2] })
-    protected _polygon: cc.Vec2[] = [new cc.Vec2(-100, -100), new cc.Vec2(0, 100), new cc.Vec2(100, -100)];
+    protected _polygon: cc.Vec2[] = [new cc.Vec2(-100, -100), new cc.Vec2(100, -100), new cc.Vec2(100, 100), new cc.Vec2(-100, 100)];
     @property({ type: [cc.Vec2] })
     get polygon()
     {
@@ -17,6 +36,9 @@ export default class PolygonSprite extends cc.Sprite
         // @ts-ignore
         this.setVertsDirty();
     }
+
+    @property
+    editing: boolean = false;
 
     public _flushAssembler(): void
     {
