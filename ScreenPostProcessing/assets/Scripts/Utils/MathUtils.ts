0cb7046319f300896e84aaa284dcc206c8274aed
shuxiang <shux21091301@gulangkj.com>
2022-04-26 12:57:31 +0800

    feat 新增 矢量压缩算法-光栏法，适合压缩graphic绘制所需点的轨迹

--- ScreenPostProcessing/assets/Scripts/Utils/MathUtils.ts
+++ ScreenPostProcessing/assets/Scripts/Utils/MathUtils.ts
@@ -1,3 +1,6 @@
+type Vec2 = cc.Vec2;
+const Vec2 = cc.Vec2;
+
 export class MathUtils 
 {
     /** 耳切法，将多边形分解为多个三角形，获取切除凹顶点后的顶点 */
@@ -102,5 +105,156 @@ export class MathUtils
             );
         });
         return uvs;
+    }
+
+    /**
+     * 旋转二维向量
+     * @param angle 弧度
+     */
+    public static  rotateVec2(p: Vec2, angle: number): Vec2
+    {
+        return new Vec2(
+            Math.cos(angle) * p.x - Math.sin(angle) * p.y,
+            Math.sin(angle) * p.x + Math.cos(angle) * p.y
+        );
+    }
+
+    /**
+     * 矢量压缩算法-光栏法
+     * 适合绘画路径时，压缩路径的点
+     * @param caliber 扇形口径 
+     */
+    public static simplifyLightBar(points: Vec2[], caliber: number = 3): Vec2[]
+    {
+        if (caliber <= 0)
+        {
+            return points;
+        }
+
+        if (points.length < 2)
+        {
+            return points;
+        }
+
+        /**
+        * 点是否在光栏内
+        * @param p1 光栏起始点
+        */
+        const isInLightBar = function (up: Vec2, down: Vec2, p1: Vec2, target: Vec2): boolean
+        {
+            let line: Vec2 = target.sub(p1);
+            if (line.cross(up) >= 0 && line.cross(down) <= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // 获取光栏上下两条边向量
+        const getLightBarEdge = function (p1: Vec2, p2: Vec2, caliber: number)
+        {
+            // 旋转向量
+            const rotateVec2 = function (p: Vec2, angle: number): Vec2
+            {
+                return new Vec2(
+                    Math.cos(angle) * p.x - Math.sin(angle) * p.y,
+                    Math.sin(angle) * p.x + Math.cos(angle) * p.y
+                );
+            }
+            let len = Vec2.distance(p1, p2);
+            let angle = Math.atan2(caliber * 0.5, len);
+            let up = rotateVec2(p2.sub(p1), angle);
+            let down = rotateVec2(p2.sub(p1), -angle);
+            return { up, down };
+        }
+
+        const acosVector = function (v1: Vec2, v2: Vec2)
+        {
+            return Math.acos(v1.dot(v2) / (v1.mag() * v2.mag()));
+        }
+
+        for (let p of points)
+        {
+            p["__valid__"] = true;
+        }
+
+        let p1 = points[0];
+        let p2 = points[1];
+        let light = getLightBarEdge(p1, p2, caliber);
+        let up = light.up;
+        let down = light.down;
+        let lastIndex = 1;
+
+        for (let i = 2; i < points.length; i++)
+        {
+            let p = points[i];
+            if (isInLightBar(up, down, p1, p))
+            {
+                // 如果下一个点在光栏内，则删除上一个点，当前点为新p2
+                points[i - 1]["__valid__"] = false;
+                p2 = points[i];
+                let light = getLightBarEdge(p1, p2, caliber);
+                let newUp = light.up, newDown = light.down;
+
+                // 开始缩小光栏口径
+                let p1p2: Vec2 = p2.sub(p1);
+
+                // 新的点与旧的扇形的起始点p1产生的新扇形的两条边与旧的扇形两条边，一共四条边，取中间夹角最小的两条边作为新的边
+                // 也就是缩小光栏的口径
+                if (acosVector(p1p2, newDown) < acosVector(p1p2, down))
+                {
+                    down = newDown;
+                }
+
+                if (acosVector(p1p2, newUp) < acosVector(p1p2, up))
+                {
+                    up = newUp;
+                }
+            } else
+            {
+                // 如果不在，则保留上一个点，以上一个点为新p1
+                points[i - 1]["__valid__"] = true;
+                p1 = points[i - 1];
+                p2 = points[i];
+                let light = getLightBarEdge(p1, p2, caliber);
+                up = light.up;
+                down = light.down;
+
+                // 上一个有效点到现在有效点直线， 之间如果有回折，那最末端的点应该保留
+                let lastPoint = points[lastIndex];
+                let maxDis = 0;
+                let maxDisIndex = 0;
+
+                for (let i = lastIndex; i > 0; i--)
+                {
+                    let dis = Vec2.distance(lastPoint, points[i]);
+                    if (dis > maxDis)
+                    {
+                        maxDis = dis;
+                        maxDisIndex = i;
+                    }
+                }
+
+                if (maxDis - Vec2.distance(lastPoint, p1) > caliber)
+                {
+                    points[maxDisIndex]["__valid__"] = true;
+                }
+                lastIndex = i - 1;
+            }
+        }
+
+        let ret: Vec2[] = [];
+
+        for (let p of points)
+        {
+            if (p["__valid__"])
+            {
+                ret.push(new Vec2(p.x, p.y));
+            }
+            delete p["__valid__"];
+        }
+
+        return ret;
     }
 }
