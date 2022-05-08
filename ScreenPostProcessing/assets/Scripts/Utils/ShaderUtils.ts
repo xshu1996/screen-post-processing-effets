758c0d2c9837770fbbc8da95a552dfed4dc36429
xshu <825947170@qq.com>
2022-05-08 16:15:16 +0800

    feat 新增SDF Effect

--- ScreenPostProcessing/assets/Scripts/Utils/ShaderUtils.ts
+++ ScreenPostProcessing/assets/Scripts/Utils/ShaderUtils.ts
@@ -16,4 +16,29 @@ export class ShaderUtils
         asset.__onLoadInvoked__ = true;
         return cc.Material.create(asset, 0);
     }
+
+    /**
+     * 创建宽高为2的单色 texture
+     * @returns 
+     */
+    public static createSingleTexture(): cc.Texture2D 
+    {
+        const data: Uint8Array = new Uint8Array(2 * 2 * 4);
+        for (let i = 0; i < 2; ++i) 
+        {
+            for (let j = 0; j < 2; ++j) 
+            {
+                data[i * 2 * 4 + j * 4 + 0] = 255;
+                data[i * 2 * 4 + j * 4 + 1] = 255;
+                data[i * 2 * 4 + j * 4 + 2] = 255;
+                data[i * 2 * 4 + j * 4 + 3] = 255;
+            }
+        }
+        
+        const texture = new cc.Texture2D();
+        texture.name = "single color";
+        texture.initWithData(data, cc.Texture2D.PixelFormat.RGBA8888, 2, 2);
+        
+        return texture;
+    }
 }
