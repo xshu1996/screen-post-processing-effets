99666828b8c483a979ef3f0355107fcbe6009691
shuxiang <shux21091301@gulangkj.com>
2022-06-02 16:19:18 +0800

    feat: base64和arraybuffer转换函数

--- ScreenPostProcessing/assets/Scripts/Utils/Utils.ts
+++ ScreenPostProcessing/assets/Scripts/Utils/Utils.ts
@@ -14,4 +14,39 @@ export class Utils
     {
         return Number(Math.random().toString().substr(3, len) + Date.now()).toString(36);
     }
+
+    /**
+     * base64 数据转 Uint8Array
+     */
+    public static base64ToUint8Array(base64Str: string): Uint8Array
+    {
+        const padding = "=".repeat((4 - base64Str.length % 4) % 4);
+        const base64 = (base64Str + padding)
+            .replace(/\-/g, "+")
+            .replace(/_/g, "/");
+
+        const rawData = window.atob(base64);
+        const outputArray = new Uint8Array(rawData.length);
+
+        for (let i = 0; i < rawData.length; ++i)
+        {
+            outputArray[i] = rawData.charCodeAt(i);
+        }
+        return outputArray;
+    }
+
+    /**
+     *  ArrayBuffer 转 base64
+     */
+    public static arrayBufferToBase64(buffer: ArrayBuffer): string
+    {
+        let binary = "";
+        let bytes = new Uint8Array(buffer);
+        let len = bytes.byteLength;
+        for (let i = 0; i < len; i++)
+        {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        return window.btoa(binary);
+    }
 }
