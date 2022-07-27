72566743ef956d0e4e1c7726b47d06dd695224d1
shuxiang <shux21091301@gulangkj.com>
2022-07-27 18:11:40 +0800

    feat: 云层新增content size 控制

--- ScreenPostProcessing/assets/Scripts/shader_control/Cloud2D.ts
+++ ScreenPostProcessing/assets/Scripts/shader_control/Cloud2D.ts
@@ -20,19 +20,27 @@ export default class Cloud2D extends cc.Component
     public barCloudAlpha: cc.Slider = null;
     private _cloudAlpah: number = 8;
 
+    @property(cc.Slider)
+    public barWith: cc.Slider = null;
+
+    @property(cc.Slider)
+    public barHeight: cc.Slider = null;
+
     @property(cc.Size)
     public _cloudResolution: cc.Size = cc.size(800, 450);
 
     @property
     public get cloudResolution(): cc.Size
     {
-        return this._cloudResolution;
+        return this._cloudResolution.clone();
     }
 
     public set cloudResolution(v: cc.Size)
     {
         this._cloudResolution = v;
         this.node.setContentSize(v);
+        this._setSliderValue(this.barWith, v.width, [0, cc.view.getDesignResolutionSize().width], "宽度");
+        this._setSliderValue(this.barHeight, v.height, [0, cc.view.getDesignResolutionSize().height], "高度");
     }
 
     private _render: cc.Sprite = null;
@@ -49,6 +57,8 @@ export default class Cloud2D extends cc.Component
         this._setSliderValue(this.barSpeed, this._speed, [0, 0.5], "云速度");
         this._setSliderValue(this.barCloudAlpha, this._cloudAlpah, [0, 16], "云透明度");
         this._setSliderValue(this.barCloudScale, this._cloudScale, [0.1, 2], "云体积");
+        this._setSliderValue(this.barWith, this.cloudResolution.width, [0, cc.view.getDesignResolutionSize().width], "宽度");
+        this._setSliderValue(this.barHeight, this.cloudResolution.height, [0, cc.view.getDesignResolutionSize().height], "高度");
     }
 
     protected onEnable(): void
@@ -89,6 +99,18 @@ export default class Cloud2D extends cc.Component
             let scale = this.barCloudScale.progress * (2 - 0.1) + 0.1;
             this._setSliderValue(this.barCloudScale, scale, [0.1, 2], "云体积");
             this._render.getMaterial(0).setProperty("cloudScale", scale);
+        }, this);
+
+        this.barWith.handle.node.on(cc.Node.EventType.TOUCH_END, () =>
+        {
+            let width = this.barWith.progress * cc.view.getDesignResolutionSize().width;
+            this.cloudResolution = cc.size(width, this.cloudResolution.height);
+        }, this);
+
+        this.barHeight.handle.node.on(cc.Node.EventType.TOUCH_END, () =>
+        {
+            let height = this.barHeight.progress * cc.view.getDesignResolutionSize().height;
+            this.cloudResolution = cc.size(this.cloudResolution.width, height);
         }, this);
     }
 
